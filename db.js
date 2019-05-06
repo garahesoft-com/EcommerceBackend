78b100f6556a9e81f36a625105196d8469803a6b
Gerald Selvino <gnsapp2k18@tutanota.com>
2019-05-06 16:42:59 +0800

    DB access modules

---
+++ db.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+var config = require('./dbconfig.js');
+
+var connObj = { 
+    client: null, 
+    status: "disconnected",
+    connect: function () {
+        //Keep trying to connect to database every 3 seconds
+        //until the connection becomes successful
+        var intervalhandle = setInterval(function() {
+            var client = new mysql.createConnection(config);
+            client.connect(function(err) {
+                if (err) {
+                    console.error("Unable to connect to database");
+                    client.end();
+                    return;
+                }
+                clearInterval(intervalhandle);
+                connObj.client = client;
+                connObj.status = "connected";
+                console.log ("Connection to MySql database @ " + config.host + ":" + config.port + " is successful.");
+            });
+        }, 3000);
+    }
+};
+
+module.exports = connObj;
---
+++ dbconfig.js
@@ -0,0 +1,10 @@
+var config = {
+    user: "turing-db-user", 
+    database: "turing_backend", 
+    password: "turing-DB-pass123456", 
+    host: 'localhost',
+    port: 3306, 
+    connectionLimit: 10
+};
+
+module.exports = config;
